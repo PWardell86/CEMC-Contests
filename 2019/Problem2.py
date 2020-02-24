5dd8247610177c447181676dd1685ecc32a5b5df
PWardell86 <peterwardell86@gmail.com>
2020-02-24 18:33:11 -0500

    A few edits to make it faster. Timer added

--- 2019/Problem2.py
+++ 2019/Problem2.py
@@ -1,13 +1,17 @@
 import math
+import time
 
-#Find all primes below 1000
+#Find primes needed for the question
 primes = []
 isPrime = 0
-for primeCounter in range(1, 2000):
+for primeCounter in range(2, 1994):
     isPrime = 0
     for primeCounter2 in range(1, primeCounter + 1):
         if primeCounter % primeCounter2 == 0:
             isPrime += 1
+
+        if isPrime > 2:
+            break
 
     if isPrime == 2:
         primes.append(primeCounter)
@@ -16,20 +20,21 @@ def FindPrimes(mean):
     for primeNum in primes:
         if primeNum > 2 * mean:
             break
-        
+
         prime2 = ((2 * mean) - primeNum)
         if prime2 in primes:
             return [primeNum, prime2]
-        
-    
 
-
+t1 = time.time()     
 for number in range(1, 1000):
     averagePrimes = FindPrimes(number)
     if averagePrimes is not None:
         print("%s: %s %s" % (number, averagePrimes[0], averagePrimes[1]))
     else:
         print("Not possible for %s" % (number))
+t2 = time.time()
+
+print("That took %s s" % (round(t2 - t1, 3)))
 
 
 
